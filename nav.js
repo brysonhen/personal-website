7fdc16222288a73ae681eb127f567ddf62f8b298
Bryson Henderson <bryshenders@gmail.com>
2026-05-11 07:03:52 -0500

    Back button exits site

--- nav.js
+++ nav.js
@@ -16,4 +16,15 @@
   }, { rootMargin: '-40% 0px -40% 0px' });
 
   sections.forEach(function (s) { observer.observe(s); });
+
+  // Use replaceState instead of pushState so anchor clicks don't pollute history
+  document.querySelectorAll('a[href^="#"]').forEach(function (link) {
+    link.addEventListener('click', function (e) {
+      const target = document.querySelector(this.getAttribute('href'));
+      if (!target) return;
+      e.preventDefault();
+      target.scrollIntoView({ behavior: 'smooth' });
+      history.replaceState(null, '', this.getAttribute('href'));
+    });
+  });
 })();
